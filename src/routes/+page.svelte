1f7c391a95caf920c0e0e20e4c5727f17aaded50
ProfSathishR <sathishse13@gmail.com>
2023-03-17 18:41:39 +0530

    Added successfully

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- <h1>Welcome to Sathish Portfolio</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+-->
+
+<img src="https://sites.google.com/site/profsathishwebsite/_/rsrc/1488350254416/home/sad.jpg" alt="Image of Professor Sathish">
+<h1>Professor Sathish</h1>
+<p>I'am a python staff</p>
